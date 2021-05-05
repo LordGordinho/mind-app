01cfb776f14fc94ff068b84a66914706958bd3a8
Jorge Felipe <jfss1@ifal.edu.br>
2021-05-05 16:36:57 -0300

    CRUD Criado

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,7 +1,52 @@
 class ContentsController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_content, only: [:show, :edit, :update, :destroy]
 
     def index
         @contents = current_user.contents
+    end
+
+    def show
+    end
+
+    def new
+        @content = Content.new
+    end
+
+    def create
+        @content = current_user.contents.build(content_params)
+        if @content.save
+            redirect_to contents_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+
+        if @content.update(content_params)
+            redirect_to contents_path
+        else
+            render :edit
+        end
+    end
+    
+    def destroy
+        @content.destroy
+
+        redirect_to contents_path
+    end
+
+    private
+
+    def set_content
+        @content = Content.find(params[:id])
+    end
+
+    def content_params
+        params.require(:content).permit(:title, :description)
     end
 end
