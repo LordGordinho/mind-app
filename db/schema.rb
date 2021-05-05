414cc3dcfc1a21ce11e353f061d9516d3cfcf7a3
Jorge Felipe <jfss1@ifal.edu.br>
2021-05-05 06:48:34 -0300

    Adicionando Listagem de Conteudo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_203929) do
+ActiveRecord::Schema.define(version: 2021_05_05_011602) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tag_contents", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "content_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_tag_contents_on_content_id"
+    t.index ["tag_id"], name: "index_tag_contents_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name", "user_id"], name: "index_tags_on_name_and_user_id", unique: true
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +51,8 @@ ActiveRecord::Schema.define(version: 2021_05_04_203929) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contents", "users"
+  add_foreign_key "tag_contents", "contents"
+  add_foreign_key "tag_contents", "tags"
+  add_foreign_key "tags", "users"
 end
